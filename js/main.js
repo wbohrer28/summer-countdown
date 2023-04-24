a8f76828733d885e4ac7bfc5702eb602e937aecc
will bohrer <124627795+wbohrer28@users.noreply.github.com>
2023-04-24 10:44:49 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,69 +1,99 @@
-var pcsdEndDate = new Date("2023-06-05 15:40").getTime();
+var pcsdEndDate = new Date("2023-06-05 15:40")
 
-var pcsdResetTime = setInterval(function() {
-    var timeNow = new Date().getTime();
-    var timeLeft = (pcsdEndDate - timeNow);
+setInterval(function() {
+  if (document.getElementById("pcsdWeekendCheck").checked == true) {
+    var finalTime = calculateTime(pcsdEndDate, true);
 
-    var days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var mins = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
-    var secs = Math.floor((timeLeft % (1000 * 60)) / 1000);
+    document.getElementById("pcsdTime").innerHTML = finalTime;
+  } else {
+    var finalTime = calculateTime(pcsdEndDate, false);
 
-    var formattedDays = days.toLocaleString("en-US", {
-        minimumIntegerDigits: 2,
-        useGrouping: false
-    });
-    var formattedHours = hours.toLocaleString("en-US", {
-        minimumIntegerDigits: 2,
-        useGrouping: false
-    });
-    var formattedMins = mins.toLocaleString("en-US", {
-        minimumIntegerDigits: 2,
-        useGrouping: false
-    });
-    var formattedSecs = secs.toLocaleString("en-US", {
-        minimumIntegerDigits: 2,
-        useGrouping: false
-    });
+    document.getElementById("pcsdTime").innerHTML = finalTime;
+  }
 
-    document.getElementById("pcsdTime").innerHTML = (formattedDays + ":" + formattedHours + ":" + formattedMins + ":" + formattedSecs);
-}, 1000);
+  var timeInput = document.getElementById("customDate").value;
+  
+  if (timeInput) {
+    var customEndDate = new Date(timeInput);
 
-var customResetTime = setInterval(function() {
-    var input = document.getElementById("customDate").value;
-    var endDate = new Date(input).getTime();
-
-    if (endDate) {
-        var timeNow = new Date().getTime();
-        var timeLeft = (endDate - timeNow);
-        var nowDate = new Date(timeNow).getDate();
-    
-        var days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var mins = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
-        var secs = Math.floor((timeLeft % (1000 * 60)) / 1000);
-    
-        var formattedDays = days.toLocaleString("en-US", {
-            minimumIntegerDigits: 2,
-            useGrouping: false
-        });
-        var formattedHours = hours.toLocaleString("en-US", {
-            minimumIntegerDigits: 2,
-            useGrouping: false
-        });
-        var formattedMins = mins.toLocaleString("en-US", {
-            minimumIntegerDigits: 2,
-            useGrouping: false
-        });
-        var formattedSecs = secs.toLocaleString("en-US", {
-            minimumIntegerDigits: 2,
-            useGrouping: false
-        });
-
-        var finalTime = (formattedDays + ":" + formattedHours + ":" + formattedMins + ":" + formattedSecs)
-    
-        document.getElementById("customTime").innerHTML = (finalTime);
+    if (document.getElementById("customWeekendCheck").checked == true) {
+      var finalTime = calculateTime(customEndDate, true);
+  
+      document.getElementById("customTime").innerHTML = finalTime;
     } else {
-        document.getElementById("customTime").innerHTML = ("00:00:00:00");
-    };
+      var finalTime = calculateTime(customEndDate, false);
+  
+      document.getElementById("customTime").innerHTML = finalTime;
+    }
+  } else {
+    document.getElementById("customTime").innerHTML = "00:00:00:00";
+  }
 }, 1000);
+
+function calculateTime(endDate, includeWeekends) {
+  var timeNow = new Date()
+  var timeLeft = (endDate.getTime() - timeNow.getTime());
+
+  var days
+  var hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var mins = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+  var secs = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+  if (includeWeekends) {
+    days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+  } else {
+    days = calculateWeekdays(timeNow, endDate)
+  }
+
+  var formattedDays = days.toLocaleString("en-US", {
+    minimumIntegerDigits: 2,
+    useGrouping: false
+  });
+  var formattedHours = hours.toLocaleString("en-US", {
+    minimumIntegerDigits: 2,
+    useGrouping: false
+  });
+  var formattedMins = mins.toLocaleString("en-US", {
+    minimumIntegerDigits: 2,
+    useGrouping: false
+  });
+  var formattedSecs = secs.toLocaleString("en-US", {
+    minimumIntegerDigits: 2,
+    useGrouping: false
+  });
+
+  return (formattedDays + ":" + formattedHours + ":" + formattedMins + ":" + formattedSecs);
+}
+
+function calculateWeekdays(preStartDate, preEndDate) {
+  var startDate = new Date(preStartDate);
+  var endDate = new Date(preEndDate);
+
+  var millisecondsPerDay = 86400 * 1000;
+
+  startDate.setHours(0, 0, 0, 1);
+  endDate.setHours(23, 59, 59, 999);
+
+  var difference = endDate - startDate;
+  var days = Math.ceil(difference / millisecondsPerDay);
+
+  var weeks = Math.floor(days / 7);
+  days -= weeks * 2;
+
+  var startDay = startDate.getDay();
+  var endDay = endDate.getDay();
+
+  if (startDay - endDay > 1) {
+    days -= 2;
+  }
+
+  if (startDay == 0 && endDay != 6) {
+    days--;  
+  }
+
+  if (endDay == 6 && startDay != 0) {
+    days--;
+  }
+
+  return days;
+};
